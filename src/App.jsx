e25073d5db675cb4c1573c90bcc8c0ef85987a9e
Ayush Chauhan <ayushchauhan212@gmail.com>
2024-02-19 21:05:35 +0530

    files added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {Homepage} from "./components/Homepage"
+import {About} from "./components/About"
+
+function App() {
+  return (
+    <div>
+      {/* <Homepage></Homepage> */}
+      <About></About>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/About.jsx
@@ -0,0 +1,7 @@
+function About() {
+    return (
+        <div>
+            <h1>loda lasun</h1>
+        </div>
+    )
+}
