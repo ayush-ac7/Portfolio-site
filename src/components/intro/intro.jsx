c9ffaa450dc8cc2d62f593023a2973cdefb8180b
Ayush Chauhan <ayushchauhan212@gmail.com>
2024-03-05 19:53:22 +0530

    intro section modified

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -10,7 +10,7 @@ export default function Intro() {
                 <span className="hello">Hello</span>
                 <span className="introText">I'm <span className="introName">Ayush</span> <br/>Frontend Developer </span>
                 <p className="introPara">I am a Web developer passionate about creating <br/> visually appealing and user friendly website.</p>
-                <Link><button className="btn"><img src={btnImg} alt="Hire me" className="btnImg" /> Hire Me</button></Link>
+                {/* <Link><button className="btn"><img src={btnImg} alt="Hire me" className="btnImg" /> Hire Me</button></Link> */}
             </div>
             <img src={bg} alt="Profile" className="bg" />
         </section>
