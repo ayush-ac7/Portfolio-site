b08c261ae0f302172cd34591bd83d02abae3952c
Ayush Chauhan <ayushchauhan212@gmail.com>
2024-03-17 21:45:51 +0530

    worked on projects section

--- src/components/Works/works.jsx
+++ src/components/Works/works.jsx
@@ -1,10 +1,26 @@
 import './works.css';
+import project1 from "../../assets/univ-homepage.png"
+import project2 from "../../assets/project2.png"
+import Button from '@mui/material/Button';
 
 export default function Works() {
     return (
        <section id="works">
            <h2 className="worksTitle">My Portfolio</h2>
-           <span className="worksDesc">WORK IN PROGRESS!</span>
+           <div className="projects">
+              <div className="project">
+                <img src={project1} alt="" className="proj-img" />
+                <a className='proj-btn' href="https://university-website-alpha.vercel.app/">
+                <Button variant="contained" size="large" type="submit" value= "Send" className="submitBtn">Live Demo</Button>
+                </a>
+              </div>
+              <div className="project">
+              <img src={project2} alt="" className="proj-img" />
+                <a className='proj-btn' href="https://spotify-clone-omega-five.vercel.app/">
+                <Button variant="contained" size="large" type="submit" value= "Send">Live Demo</Button>
+                </a>
+              </div>
+           </div>
        </section> 
     );
 }
